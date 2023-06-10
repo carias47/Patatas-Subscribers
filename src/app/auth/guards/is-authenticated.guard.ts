b35c8df9d5ef19864aea255164cfa2d966400afe
Cristian Arias <ariascristian085@gmail.com>
2023-06-09 21:25:48 -0500

    Login guard agregado

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -1,10 +1,28 @@
-// import { Injectable } from '@angular/core';
-// import { CanActivate, CanLoad, Router } from '@angular/router';
-// import { AuthService } from '../services/auth.service';
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class isAuthenticatedGuard implements CanActivate, CanLoad {
-//   constructor(private authService: AuthService, private router: Router) {}
-
-// };
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { TokenService } from '../services/token.service';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class isAuthenticatedGuard implements CanActivate {
+  constructor(private router: Router, private tokenService: TokenService) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.tokenService.isLogged()) {
+      this.router.navigate(['/subscribers']);
+      return false;
+    }
+    return true;
+  }
+}
